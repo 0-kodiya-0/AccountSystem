7bd0ccd3cd81fc85c74604cc2fcd25f8102a68ae
0-kodiya-0 <sanithujayafiverr@gmail.com>
2025-06-06 23:48:33 +0530

    refactor(auth-react-sdk): Removed unnessary file

--- packages/sdk/auth-react-sdk/src/hooks/useCurrentAccount.ts
+++
@@ -1,9 +0,0 @@
-import { UseAccountOptions } from "../types";
-import { useAccount } from "./useAccount";
-
-/**
- * Convenience hook for the current account
- */
-export const useCurrentAccount = (options?: UseAccountOptions) => {
-    return useAccount(undefined, options);
-};
