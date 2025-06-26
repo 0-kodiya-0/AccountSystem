10dc04448318d7bf8a17865011d1c8bb128e0d96
0-kodiya-0 <sanithujayafiverr@gmail.com>
2025-06-26 21:53:33 +0530

    fix(auth-node-sdk): Added `SocketService` export to index.ts

--- packages/sdk/auth-node-sdk/src/index.ts
+++ packages/sdk/auth-node-sdk/src/index.ts
@@ -1,6 +1,7 @@
 export { ApiSdk, ApiSdkConfig } from './middleware/Auth.middleware';
 export { HttpClient } from './client/HttpClient';
 export { ApiService } from './services/ApiService';
+export { SocketService } from './services/SocketService';
 export { SocketClient } from './client/SocketClient';
 
 // Export all types
