95ff73d1a8ab7c416dc48c1a11f5a520d7d50e51
0-kodiya-0 <sanithujayafiverr@gmail.com>
2025-07-23 22:29:05 +0530

    refactor(auth-react-sdk, hook): remove unnecessary code

--- packages/sdk/auth-react-sdk/src/hooks/useTwoFactorAuth.ts
+++ packages/sdk/auth-react-sdk/src/hooks/useTwoFactorAuth.ts
@@ -1,5 +1,4 @@
 import { useState, useCallback, useEffect } from 'react';
-import { useSession } from './useSession';
 import { useAppStore } from '../store/useAppStore';
 import { useAuthService } from '../context/ServicesProvider';
 import {
